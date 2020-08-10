021630f223958972ecdb3a484c5f09dbcad60bb2
Kyle Masterson <kylemasterson25@gmail.com>
2020-08-10 16:04:46 -0400

    meaningful message

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def answer
+    @question = params[:question]
+    @response = if @question.downcase == 'i am going to work right now!'
+                  'great'
+                elsif @question.end_with?('?')
+                  'Silly question, get dressed and go to work!'
+                else
+                  "I don't care, get dressed and go to work!"
+                end
+  end
+end
